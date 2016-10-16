f1946bf23d7cd3e4f08dfc4b0c7fb47b20f084e2
Jeremy <saturn5971@gmail.com>
2016-10-16 10:18:31 -0400

    Added "Matrix" and "PositionMatrix" classes
    Classes handle matrix multiplication and scaling/rotation/projection,
    etc.

--- lib.js
+++ lib.js
@@ -1,3 +1,6 @@
+// used tutorials on http://webglfundamentals.org to make these
+
+
 function createShader(gl, type, source) {
     const shader = gl.createShader(type);
     gl.shaderSource(shader, source);
@@ -20,4 +23,143 @@ function createProgram(gl, vertexShader, fragmentShader) {
     
     console.log(gl.getProgramInfoLog(program));
     gl.deleteProgram(program);
+}
+
+class Matrix {
+    constructor(w, h, initial) {
+        if (initial === undefined) {
+            initial = Array(w * h).fill(0);
+        }
+        if (initial.length != w * h) {
+            throw 'Initial value isn\'t the right length'; 
+        }
+        this.w = w;
+        this.h = h;
+        this.m = initial;
+    }
+
+    static multiply(m1, m2) {
+        if (m1.w != m2.h) {
+            throw `Cannot multiply a matrix with width ${m1.w} by a matrix with height ${m2.h}`;
+        }
+        const newM = [];
+        for (let y = 0; y < m1.h; y++) {
+            for (let x = 0; x < m2.w; x++) {
+                let e = 0;
+                for (let i = 0; i < m1.h; i++) {
+                    e += m1.get(i, y) * m2.get(x, i);
+                }
+                newM.push(e);
+            }
+        }
+        return new Matrix(m2.w, m1.h, newM); 
+    }
+
+    get(x, y) {
+        return this.m[x + this.w * y];
+    }
+
+    set(x, y, val) {
+        this.m[x + this.w * y] = val;
+    }
+
+    multiply_update(m2) {
+        this.m = Matrix.multiply(this, m2).m;
+    }
+
+    show() {
+        for (let y = 0; y < this.h; y++) {
+            let s = '';
+            for (let x = 0; x < this.w; x++) {
+                s += this.get(x, y) + ' ';
+            }
+            console.log(s);
+        }
+    }
+}
+
+class PositionMatrix extends Matrix {
+    constructor(initial) {
+        super(4, 4, initial);
+        this.projectionMatrix = PositionMatrix.getIdentity();
+    }
+
+    static getIdentity() {
+        return new Matrix(4, 4, [
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1
+        ]);
+    }
+
+    setPerspective(fov, aspect, near, far) {
+        // https://unspecified.wordpress.com/2012/06/21/calculating-the-gluperspective-matrix-and-other-opengl-matrix-maths/
+        const f = Math.tan(Math.PI * 0.5 - 0.5 * fov);
+        const rangeInv = 1.0 / (near - far);
+        this.projectionMatrix = new Matrix(4, 4, [
+            f / aspect, 0, 0, 0,
+            0, f, 0, 0,
+            0, 0, (near + far) * rangeInv, -1,
+            0, 0, near * far * rangeInv * 2, 0,
+        ]);
+    }
+
+    translate(tx, ty, tz) {
+        this.multiply_update(new PositionMatrix([
+            1, 0, 0, 0,
+            0, 1, 0, 0, 
+            0, 0, 1, 0,
+            tx, ty, tz, 1
+        ]));
+    }
+ 
+    scale(sx, sy, sz) {
+        this.multiply_update(new PositionMatrix([
+            sx, 0, 0, 0,
+            0, sy, 0, 0,
+            0, 0, sz, 0,
+            0, 0, 0, 1,
+        ]));
+    }
+
+    rotateX(angle) {
+        const c = Math.cos(angle);
+        const s = Math.sin(angle);
+
+        this.multiply_update(new PositionMatrix([
+            1, 0, 0, 0,
+            0, c, s, 0,
+            0, -s, c, 0,
+            0, 0, 0, 1
+        ]));
+    }
+
+    rotateY(angle) {
+        const c = Math.cos(angle);
+        const s = Math.sin(angle);
+
+        this.multiply_update(new PositionMatrix([
+            c, 0, -s, 0,
+            0, 1, 0, 0,
+            s, 0, c, 0,
+            0, 0, 0, 1
+        ]));
+    }
+
+    rotateZ(angle) {
+        const c = Math.cos(angle);
+        const s = Math.sin(angle);
+
+        this.multiply_update(new PositionMatrix([
+            c, s, 0, 0,
+            -s, c, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1,
+        ]));
+    }
+
+    getProjection() {
+        return Matrix.multiply(this, this.projectionMatrix).m;
+    }
 }
